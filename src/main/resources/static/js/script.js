17fd3185fa5100ba4730eb10232d800d96d57377
chistyakov <langlydes@gmail.com>
2020-09-25 11:11:42 +0300

    copy link

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -22,4 +22,19 @@ $(document).ready(function () {
     $("#process_btn").click(function () {
         shortenUrl();
     });
+
+    $("#basic-addon2").click(function(){
+       let copyText = $("#text_result");
+       copyText.select();
+       document.execCommand("copy");
+        if (window.getSelection) {
+            if (window.getSelection().empty) {  // Chrome
+                window.getSelection().empty();
+            } else if (window.getSelection().removeAllRanges) {  // Firefox
+                window.getSelection().removeAllRanges();
+            }
+        } else if (document.selection) {  // IE?
+            document.selection.empty();
+        }
+    });
 });
